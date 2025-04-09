a73773d32d5ebc9dc7075c3792fb56412a3c3dec
Joshua Iruobe <joshuairuobe@gmail.com>
2025-04-09 14:23:08 +0100

    Resetting Database

--- import.php
+++ import.php
@@ -11,8 +11,11 @@ try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Drop and recreate the users table
     $pdo->exec("
-        CREATE TABLE IF NOT EXISTS users (
+        DROP TABLE IF EXISTS users;
+
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(255) NOT NULL,
             password TEXT NOT NULL,
@@ -22,21 +25,14 @@ try {
         );
     ");
 
-
+    
     $sql = <<<'SQL'
     INSERT INTO users (id, username, password, email, firstname, lastname) VALUES
-    (1, '@j.iruobe', '$2y$12$g77enCFF3SNDi0kGkZjaKOru0qn.I4oIAWVsBQnVIn0F4iodKWbhy', 'joshuairuobe@gmail.com', 'Joshua', 'Iruobe'),
-    (2, 'johndoe', '$2y$12$ctdPvRGKONO.tMfyR56ys.Ksct3lIdSlyjCeILmJ0OjgwAyglFiwa', 'johndoe@gmail.com', 'John', 'Doe'),
-    (4, 'johndee', '$2y$12$khLKE1L8jFDwoGJIJbGx2.UlPGjN0PJTeoElLlKXXYLE75y/DN3y.', 'johndee@gmail.com', 'John', 'Dee'),
-    (6, 'johndeed', '$2y$12$KeoUxVLnb9oczmIWse5aYOYMTSxN66MBEojoTRNb6Kjve4RKSMep6', 'johndeed@gmail.com', 'John', 'Deed'),
-    (7, 'jbrown', '$2y$12$qdpOL34SAe.O13fjeTjyl.Hhia5z3JVg2qXB1XEp5G1gyllL4i1Y2', 'jbrown@outlook.com', 'James', 'Brown'),
-    (8, 'johns', '$2y$12$uE7D61pjlGt/fXsujTrMYeff5q2kxlO87fGxCjFNBZJUC/vwfAQOG', 'john@gmail.com', 'John', 'Johns'),
-    (9, 'j.irobz', '$2y$12$XHZYzoU4rpTY2a3BTONov..lWlP9V1mClpoqksHR1kzESpBsssSIe', 'iruobejoshua96@gmail.com', 'Joshua', 'Iruobe'),
-    (10, 'sssss', '$2y$12$rekMs3P5otUtuiACUkFxHOcdYsYrqw5WKiKy7h4T/dA8SKaTzETrS', 'sssss@gmail.com', 'Shazza', 'Iruobe')
+    (1, 'j.irobz', '$2y$12$XHZYzoU4rpTY2a3BTONov..lWlP9V1mClpoqksHR1kzESpBsssSIe', 'joshuairuobe@gmail.com', 'Joshua', 'Iruobe')
 SQL;
 
     $pdo->exec($sql);
-    echo "Users imported successfully!";
+    echo "Users table reset and single user imported!";
 
 } catch (PDOException $e) {
     echo "DB import failed: " . $e->getMessage();

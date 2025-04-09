b418bd66b64c1476b1af148b57c5f3fad4f1e641
Joshua Iruobe <joshuairuobe@gmail.com>
2025-04-09 17:07:55 +0100

    Added more pages to F School

--- loans.php
+++ loans.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Budgeting</title>
+    <title>Loans</title>
     <link rel="stylesheet" href="styles.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
@@ -9,7 +9,7 @@
 </head>
 <body>
     <?php include 'navbar.php'; ?>
-    <h1 id="header" style="text-align: center;">Budgeting</h1>
+    <h1 id="header" style="text-align: center;">Loans</h1>
     <div class="notifications">
         <div class="notification">
             <h3>Savings Goal Reached!</h3>

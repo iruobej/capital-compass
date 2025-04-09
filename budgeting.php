83b5ec9d89c474f59b38a7ada9b5e064a5731d42
Joshua Iruobe <joshuairuobe@gmail.com>
2025-04-09 20:40:34 +0100

    Styling update

--- budgeting.php
+++ budgeting.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <?php include 'navbar.php'; ?>
-    <h1 id="header" style="text-align: center;">Budgeting Resources</h1>
+    <h1 id="header" style="text-align: center;">Budgeting Resources x </h1>
     <details class="box">
         <summary>
             <div class="summary-content">

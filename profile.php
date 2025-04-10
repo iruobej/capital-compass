8b8ecca7340c5b2d6a6d9eea419b8dd540d7d77b
Joshua Iruobe <joshuairuobe@gmail.com>
2025-04-10 19:42:14 +0100

    Fixing API scope error

--- profile.php
+++ profile.php
@@ -42,7 +42,7 @@ $username = $_SESSION['username'];
             'response_type' => 'code',
             'client_id' => TL_CLIENT_ID,
             'redirect_uri' => TL_REDIRECT_URI,
-            'scope' => 'info accounts balance transactions identity',
+            'scope' => 'openid info accounts balance transactions identity',
             'state' => $state,
             'nonce' => $nonce
         ]);

6c6ee8b5906665a9cf9d3ebfd93663a18e3b348c
Joshua Iruobe <joshuairuobe@gmail.com>
2025-04-10 17:05:10 +0100

    Added content to F School

--- budgeting.php
+++ budgeting.php
@@ -33,7 +33,7 @@
                 </li>
                 <li>
                     <h4>Book: The Budget Book for Young Adults</h4>
-                    <p>Are you a young adult who is starting out in life and want to get the most from your financial situation? </p>
+                    <p>This new book, The Budget Book for Young Adults: Guide on Budgeting Basics for Beginners, including the 50/30/20 Budget Approach, will help you to start planning your finances so that you have enough money for everything you need. </p>
                     <a href="https://www.amazon.co.uk/Budget-Book-Young-Adults-Budgeting/dp/1387886045/ref=sr_1_5?crid=2E0V19VQDEROL&dib=eyJ2IjoiMSJ9.e9iIixNpmD5ifRmKC8qSJogdHeyrsnkAmPf604A1Wxaka2OdNEE891oAJlov4mSzEtUkB_vY6LW9Orj5NT-_oNzQUVUcEJpw5EqSSOgfpyet_bHcsbuqWgesTRW9lerrkKr2PxKkBytqVV9m_absygPkMxPGJ9zs_tdOxKUXWcxvfVEf2X9OcMv3gUaKh6-Gh2pakaa0atTZnRaD9N41Okuz4TBZXrGpAreD0gOwgW4.XDAktuiqVzhYsZJdek9zogf8yNIBk24dkzUctv7VtZg&dib_tag=se&keywords=budgeting+basics&qid=1744235716&sprefix=budgeting+basics%2Caps%2C102&sr=8-5" target="_blank">Purchase Book</a>
                 </li>
             </ul>
@@ -53,17 +53,17 @@
                 <li>
                     <h4>Book: The Only Budgeting Book You'll Ever Need: How to Save Money and Manage Your Finances with a Personal Budget Plan That Works for You</h4>
                     <p>Filled with only the most essential information on budgeting, this book shows you how to build a financial plan that not only meets your needs, but helps you stay on track.</p>
-                    <a href="https://www.amazon.com/Only-Budgeting-Book-Youll-Ever/dp/1440550107?asc_source=01J3NF8C5MVTHVNEQMAHFDJRJP&tag=snxgb449-21" target="_blank"></a>
+                    <a href="https://www.amazon.com/Only-Budgeting-Book-Youll-Ever/dp/1440550107?asc_source=01J3NF8C5MVTHVNEQMAHFDJRJP&tag=snxgb449-21" target="_blank">Purchase Book</a>
                 </li>
                 <li>
-                    <h4>Financial Literacy Part 02 - Budgeting</h4>
-                    <p>Read a step-by-step guide on how to set up your first budget effectively.</p>
-                    <button>Read Article</button>
+                    <h4>Video: How Do I Make A Budget And Stick To It?</h4>
+                    <p>An informative video by Dave Ramsey explaining the mechanics behind making a budget.</p>
+                    <a href="https://www.youtube.com/watch?v=4Eh8QLcB1UQ">Watch Video</a>
                 </li>
                 <li>
-                    <h4>Budgeting for Beginners</h4>
-                    <p>An eBook that covers all the fundamentals to get you started on budgeting.</p>
-                    <button>Purchase eBook</button>
+                    <h4>Book: Budgeting Basics and Beyond</h4>
+                    <p>Budgeting Basics and Beyond, Fourth Edition equips you with an all-in-one resource guaranteed to make the budgeting process easier, less stressful, and more effective.</p>
+                    <a href="https://www.amazon.co.uk/Budgeting-Basics-Beyond-Wiley-Corporate/dp/1118096274">Purchase Book</a>
                 </li>
             </ul>
         </div>
@@ -80,19 +80,16 @@
         <div class="dropdown-content"> 
             <ul>
                 <li>
-                    <h4>Budgeting Basics!</h4>
-                    <p>See budgeting through the lens of an army general!</p>
-                    <a href="https://www.youtube.com/watch?v=sVKQn2I4HDM">Watch Video</a>
+                    <h4>Article: Beyond Budgeting</h4>
+                    <p>This scholarly article delves into the concept of Beyond Budgeting, discussing how organizations can move away from traditional budgeting methods to gain strategic advantages. It explores the principles and practical implications of adopting more adaptive and decentralized budgeting processes.</p>
+                    <a href="https://corporatefinanceinstitute.com/resources/fpa/beyond-budgeting/">Read Article</a>
                 </li>
                 <li>
-                    <h4>Your guide to creating a budget plan</h4>
-                    <p>Read a step-by-step guide on how to set up your first budget effectively.</p>
-                    <a href="https://bettermoneyhabits.bankofamerica.com/en/saving-budgeting/creating-a-budget">Read Article</a>
-                </li>
-                <li>
-                    <h4>Book: The Budget Book for Young Adults</h4>
-                    <p>Are you a young adult who is starting out in life and want to get the most from your financial situation? </p>
-                    <a href="https://www.amazon.co.uk/Budget-Book-Young-Adults-Budgeting/dp/1387886045/ref=sr_1_5?crid=2E0V19VQDEROL&dib=eyJ2IjoiMSJ9.e9iIixNpmD5ifRmKC8qSJogdHeyrsnkAmPf604A1Wxaka2OdNEE891oAJlov4mSzEtUkB_vY6LW9Orj5NT-_oNzQUVUcEJpw5EqSSOgfpyet_bHcsbuqWgesTRW9lerrkKr2PxKkBytqVV9m_absygPkMxPGJ9zs_tdOxKUXWcxvfVEf2X9OcMv3gUaKh6-Gh2pakaa0atTZnRaD9N41Okuz4TBZXrGpAreD0gOwgW4.XDAktuiqVzhYsZJdek9zogf8yNIBk24dkzUctv7VtZg&dib_tag=se&keywords=budgeting+basics&qid=1744235716&sprefix=budgeting+basics%2Caps%2C102&sr=8-5">Purchase Book</a>
+                    <h4>Course: Beyond Budgeting - Business Agility in </h4>
+                    <p>
+                        This comprehensive article discusses the Beyond Budgeting approach as a means to achieve business agility in uncertain times.
+                    </p>
+                    <a href="https://www.agile-academy.com/en/organizational-development/beyond-budgeting/"></a>
                 </li>
             </ul>
         </div>

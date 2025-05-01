04463ea0360e61d0314422c513bc4010d96d5c8f
Joshua Iruobe <joshuairuobe@gmail.com>
2025-05-01 21:36:09 +0100

    editing financial goals

--- home.php
+++ home.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+if (!isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
+    require_once 'config.php';
+    $stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
+    $stmt->execute([$_SESSION['username']]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($user) {
+        $_SESSION['user_id'] = $user['id'];
+    }
+}
 if(!isset($_SESSION['username'])){
     header("Location: index.html");
     exit();

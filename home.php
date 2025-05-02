3e8a31ab4ce8f525630d192d5deacd9cf43662ff
Joshua Iruobe <joshuairuobe@gmail.com>
2025-05-02 04:45:02 +0100

    debugging

--- home.php
+++ home.php
@@ -49,6 +49,7 @@ $goals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <?php include 'navbar.php'; ?>
     <h1 id="header">Welcome, <?= htmlspecialchars($_SESSION['firstname'] . ' ' . $_SESSION['lastname']) ?></h1>
     <h2 style="text-align: center;">Your Badge Level: <?php echo "<span class='badge'>$badge</span>";?></h2>
+    <h2 style="text-align: center;">Your Badge Level: <?php echo "$combinedScore";?></h2>
     <div class="page-container">
         <!--Displaying accounts-->
         <h2>Your Accounts: </h2>
